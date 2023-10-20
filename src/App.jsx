d9e9aa8c8e133b3cdd5650c7ad333973abddc31b
Hasselgren Marcus <s2300288@BC-C02GR0BDQ6LR-MacBook-Air.local>
2023-10-20 23:12:04 +0300

    2.9

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "555-123456" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [haku, setHaku] = useState("");
 
   const lisaaHenkilo = (event) => {
     event.preventDefault();
@@ -30,10 +34,24 @@ const App = () => {
     console.log(event.target.value);
     setNewNumber(event.target.value);
   };
+  const handleHaeChange = (event) => {
+    console.log(event.target.value);
+    setHaku(event.target.value);
+  };
+
+  const haettuPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(haku.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          hae: <input onChange={handleHaeChange} />
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={lisaaHenkilo}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -46,7 +64,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {haettuPersons.map((person) => (
         <p key={person.name}>
           {person.name} {person.number}
         </p>
